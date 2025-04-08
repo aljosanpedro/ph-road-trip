530e245b826d8201d74119bf5eab2fc750d58040
FlamingHerb <msbbomisol@gmail.com>
2025-04-08 23:17:53 +0800

    feat: added camera

---
+++ scripts/camera.gd
@@ -0,0 +1,66 @@
+extends Node
+@onready var camera = $Camera2D
+@onready var camera_overlay = $CameraLayer
+@onready var camera_gallery = $GalleryMenu
+@onready var camera_shutter = $CameraLayer/CameraShutter
+
+# Technically, negative to because LEFT/TOP siya na limits. But it's here as an
+# absolute value.
+const NL_X: int = 371
+const NL_Y: int = 94
+const SL_X: int = 1514 
+const SL_Y: int = 825 
+
+func _process(_delta: float) -> void:
+	#print(get_viewport().get_mouse_position())
+	if camera.enabled: camera.position = get_viewport().get_mouse_position()
+	
+func _input(_event: InputEvent) -> void:
+	# CAMERA ACTION: Open camera via CAMERA COMMAND
+	if Input.is_action_just_pressed("ui_accept"):
+		if camera.enabled: _camera_disable()
+		else: _camera_activate()
+	
+	# CAMERA ACTION: Play captures a photo via RIGHT CLICK.
+	if Input.is_action_just_pressed("camera_capture") and camera.enabled:
+		camera_shutter.hide()
+		await camera_gallery.capture_photo()
+		camera_shutter.show()
+	
+	# GALLERY ACTION: Open Gallery via Mouse Thumb 1
+	if Input.is_action_just_pressed("open_gallery"):
+		if camera_gallery.visible: camera_gallery.visible = false
+		else: camera_gallery.visible = true
+	
+	# CAMERA ACTION: Zooming in via MOUSE UP/DOWN
+	if camera.enabled:
+		camera.zoom.x = clampf(camera.zoom.x - (Input.get_axis("zoom_out", "zoom_in") * 0.1), 0.75, 2.0)
+		camera.zoom.y = clampf(camera.zoom.y - (Input.get_axis("zoom_out", "zoom_in") * 0.1), 0.75, 2.0)
+		_camera_recalculate_limit()
+
+func _camera_activate() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
+	camera_overlay.visible = true
+	camera.enabled = true
+
+func _camera_disable() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	camera_overlay.visible = false
+	camera.enabled = false
+
+## Function that recalculates the limits of a camera, especially when it is
+## zooming in/out.
+## Right: 1333 px when zoomed
+## Bottom: 773 px when zoomed
+func _camera_recalculate_limit() -> void:
+	var new_left_limit 		= -(NL_X * (1.0 / camera.zoom.x))
+	var new_top_limit 		= -(NL_Y * (1.0 / camera.zoom.y))
+	var new_right_limit 	= SL_X - ((NL_X + 12) * (1.0 - (1.0 / camera.zoom.x)))
+	var new_bottom_limit 	= SL_Y - ((NL_Y + 12) * (1.0 - (1.0 / camera.zoom.y)))
+	camera.set_limit(SIDE_LEFT, new_left_limit)
+	camera.set_limit(SIDE_TOP, new_top_limit)
+	camera.set_limit(SIDE_RIGHT, new_right_limit)
+	camera.set_limit(SIDE_BOTTOM, new_bottom_limit)
+	print("LR: ", 1.0 - (1.0 / camera.zoom.x))
+	#print("Camera Right: ", camera.limit_right, " ", camera.zoom.x - 1)
+	#print("Camera Bottom: ", camera.limit_bottom, " ", camera.zoom.y - 1)
---
+++ scripts/gallery_menu.gd
@@ -0,0 +1,40 @@
+extends CanvasLayer
+
+@onready var gallery_container = $ImageContainer
+
+var pictures: Array = [
+	
+]
+
+
+func capture_photo() -> void:
+	# TODO: Crop image https://forum.godotengine.org/t/how-to-crop-center-of-an-image/5828/2
+	await RenderingServer.frame_post_draw
+	var new_image: Image = get_viewport().get_texture().get_image()
+	new_image.resize(1152, 648)
+	
+	# Crop image by making a new image that mip maps it to OBLIVION.
+	# To anyone going to maintain this, I can assure you that IT IS NOT WORTH IT to comprehend blit_rect.
+	# TODO: Adjust ratio appropriately. SCRATCH THAT, RESIZE EXISTS.
+	
+	var cropped_image: Image = Image.new()
+	var specified_region = Rect2(371, 95, 417, 376)
+	cropped_image = Image.create(417, 376, true, new_image.get_format())
+	cropped_image.blit_rect(new_image, specified_region, Vector2(0,0))
+	
+	# Append image to the array and then call pictures.
+	pictures.append(cropped_image)
+	_create_new_image(cropped_image)
+
+func _create_new_image(new_image: Image) -> void:
+	# Create a new TextureRect and use new image taken.
+	var new_entry = TextureRect.new()
+	new_entry.texture = ImageTexture.create_from_image(new_image)
+	
+	# Set it so that the gallery works like a charm. AND NOT TAKE THE ENTIRE SPACE.
+	new_entry.custom_minimum_size = Vector2(200, 181)
+	new_entry.expand_mode = TextureRect.EXPAND_FIT_WIDTH
+	
+	# Finally, add it to gallery.
+	gallery_container.add_child(new_entry)
+	
