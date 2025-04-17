2b5b50ffe150b9ae02a377af344954e0bd923992
Michael Omisol <55610651+FlamingHerb@users.noreply.github.com>
2025-04-17 22:44:04 +0800

    feat!: added Events.enable_character_povs(bool) for Act 4 (#12)
    * feat: added enable/disable character pov switching options
    
    * fix: switched up character POV disabling accidentally in the _enable_character_pov function
    
    * chore: added test scene for debugging

--- scripts/logic/events.gd
+++ scripts/logic/events.gd
@@ -7,6 +7,7 @@ enum POV_Character {
 }
 signal change_map(path: String)
 signal open_camera_signal()
+signal enable_character_povs(value: bool)
 
 var current_pov: POV_Character = POV_Character.ADI
 #endregion
@@ -26,6 +27,14 @@ func initialize() -> void:
 func change_area(path: String) -> void:
 	change_map.emit(path)
 
+## Helper function to make code easier to read.
+func open_camera() -> void:
+	open_camera_signal.emit()
+
+#region POV Switch Functions
+#===================================================================
+# POV Switch Functions
+#===================================================================
 ## Set the current [param POV] of the game.
 func set_current_pov(value: POV_Character) -> void:
 	print("Current POV: " + POV_Character.find_key(value))
@@ -35,8 +44,10 @@ func set_current_pov(value: POV_Character) -> void:
 func get_current_pov() -> POV_Character:
 	return current_pov
 
-## Helper function to make code easier to read.
-func open_camera() -> void:
-	open_camera_signal.emit()
+## Helper function to enable/disable [param value] for POV switching.
+func scene_pov_enable(value: bool) -> void:
+	enable_character_povs.emit(value)
 	
+#endregion
+
 #endregion
