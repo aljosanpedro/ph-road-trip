2bb04f728fb1e5fca892e399a0d160a35739cb60
Aljo San Pedro <ajrsanpedro215@gmail.com>
2025-04-21 20:33:40 +0800

    scrapbook.gd: Improved code style. Added fade-in transition and auto-label text by entry node name.

--- scripts/scenes/scrapbook.gd
+++ scripts/scenes/scrapbook.gd
@@ -1,18 +1,46 @@
 extends Control
 
 
-@onready var entries : Control = $Entries
-@onready var locations : Array[Node] = entries.get_children(false)
-	# include_internal = false; no children
-
-var pictures = Events.scrapbook_pictures
+const FADE_DURATION : float = 1.5
 
 
 func _ready() -> void:
+	fade_in()
+	set_entries()
+
+
+func fade_in() -> void:
+	$Fade.visible = true
+	
+	var screen = $Fade
+	var tween = get_tree().create_tween()
+	
+	tween.tween_property(screen, "color", Color.TRANSPARENT, FADE_DURATION)
+
+
+func set_entries() -> void:
+	var entries : Array[Node] = $Entries.get_children(false)
+		# Node: Node2D
+		# include_internal = false; no children
+
+	var pictures : Array[Node] = Events.scrapbook_pictures
+		# Node: TextureRect
+	
 	for i in range(len(pictures)):
-		var location : String = locations[i].name
-		var frame : Node = get_node("Entries/" + location + "/Polaroid/Frame")
-		
+		var entry : String = entries[i].name
 		var picture : Node = pictures[i]
 		
-		frame.add_child(picture)
+		set_photo(entry, picture)
+		set_label(entry)
+
+
+func set_photo(entry, picture) -> void:
+	var frame : Node = get_node("Entries/" + entry + "/Polaroid/Frame")
+	
+	frame.add_child(picture)
+
+
+func set_label(entry) -> void:
+	var label : Node = get_node("Entries/" + entry + "/Polaroid/Frame/Label")
+		
+	label.text = entry
