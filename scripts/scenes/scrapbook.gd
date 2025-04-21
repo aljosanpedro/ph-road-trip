13d74744a08ba676b47d86a7b86c51b848517ef0
Michael Omisol <55610651+FlamingHerb@users.noreply.github.com>
2025-04-21 20:40:52 +0800

    feat: adding scrapbook prototype Resolves #4
    * import placeholder mockup, image, and font
    
    * design scrapbook entry as scene made of polaroid and text scenes
    
    * design scrapbook scene as a book with multiple entry scenes
    
    * Change scrapbook entry, polaroid, and text parent node types from Control to Node2D to better handle position
    
    * Re-introduce Control nodes for keeping general scrapbook layers in-screen
    
    * Redesigned scrapbook entries to be nodes made of components than own scenes for more flexibility
    
    * Made scrapbook.gd in scrapbook scene
    
    * Made scrapbook accessible via 'S' button
    
    * Hid picture in polaroid scrapbook component scene as placeholder
    
    * Made global scrapbook_pictures variable in global events.gd (camera -> image gallery -> events -> scrapbook)
    
    * Positioned scrapbook entries in scrapbook scene for visibility
    
    * Decided to turn off camera gallery access because output photos are freed before they can be added to the scrapbook
    
    * amend: attach file to last commit
    
    * camera.gd: commented out pictures variable code, set picture limit to # of locations, modified output image transform properties, and sent images to Events.scrapbook_pictures
    
    * First scrapbook prototype version
    
    * Set scrapbook entry text invisible for now
    
    * Converted placeholder photo from Sprite2D to TextureRect to better reflect expected child
    
    * Moved placeholder background from camera to main scene. Somehow returned movement to camera functionality.
    
    * gallery.gd: Comments on my contributions. Made a global constant for photo limit.
    
    * Removed access to gallery via button due to conflict with scrapbook picture loading
    
    * scrapbook.gd: Improved code style. Added fade-in transition and auto-label text by entry node name.
    
    ---------
    
    Co-authored-by: Aljo San Pedro <ajrsanpedro215@gmail.com>

---
+++ scripts/scenes/scrapbook.gd
@@ -0,0 +1,46 @@
+extends Control
+
+
+const FADE_DURATION : float = 1.5
+
+
+func _ready() -> void:
+	fade_in()
+	set_entries()
+
+
+func fade_in() -> void:
+	$Fade.visible = true
+	
+	var screen = $Fade
+	var tween = get_tree().create_tween()
+	
+	tween.tween_property(screen, "color", Color.TRANSPARENT, FADE_DURATION)
+
+
+func set_entries() -> void:
+	var entries : Array[Node] = $Entries.get_children(false)
+		# Node: Node2D
+		# include_internal = false; no children
+
+	var pictures : Array[Node] = Events.scrapbook_pictures
+		# Node: TextureRect
+	
+	for i in range(len(pictures)):
+		var entry : String = entries[i].name
+		var picture : Node = pictures[i]
+		
+		set_photo(entry, picture)
+		set_label(entry)
+
+
+func set_photo(entry, picture) -> void:
+	var frame : Node = get_node("Entries/" + entry + "/Polaroid/Frame")
+	
+	frame.add_child(picture)
+
+
+func set_label(entry) -> void:
+	var label : Node = get_node("Entries/" + entry + "/Polaroid/Frame/Label")
+		
+	label.text = entry
