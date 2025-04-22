aa3593e4234e0b5befa011c370a12caea3c2fc00
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2025-04-22 22:41:30 +0800

    feat: adding pause menu Resolves #11
    * Create pause scene and script
    
    * Made same edit in scrapbook-prototype brance to resolve var shadow warnings
    
    * Add pause scene to game_main under settings canvas layer node
    
    * Redesign pause scene to be composed of canvas layers to work around the pause button and screen needing to be on separate layers
    
    * Add comments on how canvas layers should ideally be ordered

--- scripts/logic/events.gd
+++ scripts/logic/events.gd
@@ -8,7 +8,6 @@ enum POV_Character {
 }
 signal change_map(path: String)
 signal open_camera_signal()
-signal enable_character_povs(value: bool)
 
 var current_pov: POV_Character = POV_Character.ADI
 
@@ -33,14 +32,6 @@ func initialize() -> void:
 func change_area(path: String) -> void:
 	change_map.emit(path)
 
-## Helper function to make code easier to read.
-func open_camera() -> void:
-	open_camera_signal.emit()
-
-#region POV Switch Functions
-#===================================================================
-# POV Switch Functions
-#===================================================================
 ## Set the current [param POV] of the game.
 func set_current_pov(value: POV_Character) -> void:
 	print("Current POV: " + POV_Character.find_key(value))
@@ -50,10 +41,8 @@ func set_current_pov(value: POV_Character) -> void:
 func get_current_pov() -> POV_Character:
 	return current_pov
 
-## Helper function to enable/disable [param value] for POV switching.
-func scene_pov_enable(value: bool) -> void:
-	enable_character_povs.emit(value)
+## Helper function to make code easier to read.
+func open_camera() -> void:
+	open_camera_signal.emit()
 	
-#endregion
-
 #endregion
