7791756ab371608f023cbd91d33cd73a0f834292
Michael Omisol <55610651+FlamingHerb@users.noreply.github.com>
2025-04-23 19:00:26 +0800

    feat: stylings for travel map (#19)
    * assets: added travel map assets
    
    * assets: added aspirational route animation
    
    * project: changed anti-alias to x8
    
    * fix: forgot to add jolli_location to final node in the map lemaw
    
    * feat: more stylistic features yadda yadda

--- scripts/scenes/map_travel.gd
+++ scripts/scenes/map_travel.gd
@@ -3,26 +3,60 @@ extends Control
 #region Initialized Variables and Exports
 signal scene_map_travel_closed
 # INFO: Locational buttons
-@onready var location_1_updqc = $UPDQC
+@onready var location_1_updqc = $"UPD-QC"
 @onready var location_2_cubao = $Cubao
 @onready var location_3_makati = $Makati
 @onready var location_4_quiapo = $Quiapo
 @onready var location_5_coast = $Coast
+@onready var location_6_jabee = $Jabee
+
+@onready var animation_player = $AnimationPlayer
+
+@onready var player_marker = $CurrentLocationPlayer
+@onready var target_area = $TargetArea
+@onready var location_label = $LocationLabel
 
 #endregion
 
+#region Virtual functions
 func _ready() -> void:
 	location_1_updqc.enable_button(true)
 	location_2_cubao.enable_button(true)
 	location_3_makati.enable_button(true)
 	location_4_quiapo.enable_button(true)
 	location_5_coast.enable_button(true)
+	location_6_jabee.enable_button(true)
+
+	# WARNING: This is Cthulhu magic at this point.
+	# Connecting the hard way.
+	for target_loc in get_tree().get_nodes_in_group("locations"):
+		target_loc.pressed.connect(_on_location_pressed.bind(target_loc))
+		target_loc.mouse_entered.connect(_on_loc_mouse_entered.bind(target_loc))
+		target_loc.mouse_exited.connect(_on_loc_mouse_exited.bind(target_loc))
+		
 
 func _on_visibility_changed() -> void:
 	if visible:
 		mouse_filter = Control.MOUSE_FILTER_STOP
+		
 	else: 
 		mouse_filter = Control.MOUSE_FILTER_IGNORE
 
-func _on_location_pressed() -> void:
+## When pressing location, change player marker location and switch scene.
+func _on_location_pressed(button: TextureButton) -> void:
+	Events.change_area(button.location.resource_path)
+	player_marker.position = Vector2(button.position.x, button.position.y)
 	scene_map_travel_closed.emit()
+
+## Basically, when hovering over a location, change target area location.
+func _on_loc_mouse_entered(button: TextureButton) -> void:
+	location_label.text = button.name
+	target_area.position = Vector2(button.position.x - 3, button.position.y - 36)
+	target_area.show()
+
+func _on_loc_mouse_exited(button: TextureButton) -> void:
+	if not Rect2(Vector2(), button.size).has_point(get_local_mouse_position()):
+		location_label.text = "???"
+		target_area.hide()
+
+#endregion
