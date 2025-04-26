a61069621302afdadc51b12740e8fdab04d3507f
FlamingHerb <msbbomisol@gmail.com>
2025-04-26 21:24:44 +0800

    feat: adding fade in/out animations for title screen

--- scripts/scenes/title_screen.gd
+++ scripts/scenes/title_screen.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	modulate = Color(0, 0, 0)
+	animation_player.play_backwards("fade")
 
 func _on_start_game_pressed() -> void:
+	animation_player.play("fade")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://scenes/scenes/game_main.tscn")
