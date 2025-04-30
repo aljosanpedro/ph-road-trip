fa752a5492ac510843f6d9cbe4dbc28b84c0e6b0
FlamingHerb <msbbomisol@gmail.com>
2025-05-01 00:06:21 +0800

    refactor: changed coloration for route saturation

--- scripts/components/map_button.gd
+++ scripts/components/map_button.gd
@@ -9,5 +9,5 @@ func _ready() -> void:
 ## Enables or disables button based on [param value].
 func enable_button(value: bool) -> void:
 	disabled = not value
-	if value: set_self_modulate(Color(1,1,1))
-	else: set_self_modulate(Color(1.5,1,1))
+	if value: set_self_modulate(Color(0,1,0))
+	else: set_self_modulate(Color(1,0,0))
