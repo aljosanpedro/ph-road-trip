27966b9feb06953c77e1886227e4370d72c328e7
FlamingHerb <msbbomisol@gmail.com>
2025-04-30 22:21:23 +0800

    assets: adding dialogue for interactables for location 1

--- scripts/locations/loc_1_updqc.gd
+++ scripts/locations/loc_1_updqc.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
 # List of interactables
-# For wiks:
-# 	kids_playing
-# 	lanterns
-# 	radio
-# For adi:
-# 	mannequins
-# 	jabee
+# for wiks:
+# **stall**
+# **sunken_garden**
+# **adis_mural**
+# for adi:
+# **trees**
+# **loose_chicken**
 
 func _ready() -> void:
 	print("Now arriving at: " + name)
-	Dialogic.start("res://assets/dialogue/intro/intro.dtl")
+	Dialogic.start("res://assets/dialogue/location_1/loc_1_scene.dtl", "intro")
 	await Dialogic.timeline_ended
--- scripts/locations/loc_2_cubao.gd
+++ scripts/locations/loc_2_cubao.gd
@@ -1,4 +1,11 @@
 extends Node2D
+# For wiks:
+# 	kids_playing
+# 	lanterns
+# 	radio
+# For adi:
+# 	mannequins
+# 	jabee
 
 func _ready() -> void:
 	print("Now arriving at: " + name)
