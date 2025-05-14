3a9ddf2cbb7e8a0d4611865b10c0f12a79a2501a
FlamingHerb <msbbomisol@gmail.com>
2025-05-14 15:12:32 +0800

    feat: added outline Resolves #38

--- scripts/components/item_base.gd
+++ scripts/components/item_base.gd
@@ -63,8 +63,10 @@ func _required_character_checker() -> bool:
 ## Allows for better info.
 func _pov_switch_grayout() -> void:
 	if not _required_character_checker():
+		if sprite_component != null: sprite_component.set_use_parent_material(false)
 		modulate = Color(0.5, 0.5, 0.5)
 	else:
+		if sprite_component != null: sprite_component.set_use_parent_material(true)
 		modulate = Color(1, 1, 1)
 		
 #endregion
