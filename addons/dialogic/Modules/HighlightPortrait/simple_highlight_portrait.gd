7f6bac400edd782966a84492ecdfabd680bd95a4
Vaccaria <55610651+FlamingHerb@users.noreply.github.com>
2025-06-17 15:35:10 +0800

    dialogue char on front (#46)
    * feat: trying character on front
    
    * feat: better positioning
    
    * feat: better positioning Resolves #45
    
    * chore: put history on top-left

--- addons/dialogic/Modules/HighlightPortrait/simple_highlight_portrait.gd
+++ addons/dialogic/Modules/HighlightPortrait/simple_highlight_portrait.gd
@@ -5,6 +5,7 @@ extends DialogicPortrait
 @export_file var image := ""
 
 var unhighlighted_color := Color.DARK_GRAY
+var _target_z_index := 98
 var _prev_z_index := 0
 
 ## Load anything related to the given character and portrait
@@ -22,9 +23,15 @@ func _ready() -> void:
 func _highlight() -> void:
 	create_tween().tween_property(self, 'modulate', Color.WHITE, 0.15)
 	_prev_z_index = DialogicUtil.autoload().Portraits.get_character_info(character).get('z_index', 0)
-	DialogicUtil.autoload().Portraits.change_character_z_index(character, 99)
+	DialogicUtil.autoload().Portraits.change_character_z_index(character, _target_z_index)
+	
+	# NOTE: WHATEVER HAPPENS, IT'S ON ME
+	DialogicUtil.autoload().Portraits.get_character_portrait(character).set_z_index(_target_z_index)
 
 
 func _unhighlight() -> void:
 	create_tween().tween_property(self, 'modulate', unhighlighted_color, 0.15)
 	DialogicUtil.autoload().Portraits.change_character_z_index(character, _prev_z_index)
+	
+	# NOTE: WHATEVER HAPPENS, IT'S ON ME
+	DialogicUtil.autoload().Portraits.get_character_portrait(character).set_z_index(_prev_z_index)
