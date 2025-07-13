8198951d21386263f5ead3969f74d8bd9d775b92
Vaccaria <msbbomisol@gmail.com>
2025-07-13 20:23:59 +0800

    refactor: added better title theme

--- scripts/scenes/title_screen.gd
+++ scripts/scenes/title_screen.gd
@@ -4,11 +4,17 @@ class_name TitleScreen
 extends Control
 
 @onready var animation_player = $AnimationPlayer
+@onready var main_menu_buttons: HBoxContainer = $MainMenuButtons
 
 func _ready() -> void:
 	AudioManager.bgm_play("res://assets/audio/bgm/title_theme.mp3")
 	modulate = Color(0, 0, 0)
-	animation_player.play_backwards("fade")
+	animation_player.play("intro")
+	
+	await animation_player.animation_finished
+	
+	for game_button: Button in main_menu_buttons.get_children():
+		game_button.disabled = false
 
 func _on_start_game_pressed() -> void:
 	AudioManager.bgm_stop(1)
