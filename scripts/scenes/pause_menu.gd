9a25e93089c441aad8da3ac5db5090e131a441f7
Vaccaria <msbbomisol@gmail.com>
2025-07-14 23:08:54 +0800

    feat: added revamped menu pause

--- scripts/scenes/pause_menu.gd
+++ scripts/scenes/pause_menu.gd
@@ -3,5 +3,25 @@
 class_name PauseMenu
 extends Control
 
+@onready var play_button: Button = $PlayButton
+@onready var pause_button: Button = $PauseButton
+@onready var pause_menu_items: Control = $PauseMenuItems
+
 func _ready() -> void:
-	pass
+	_on_play_button_pressed()	
+
+func _on_play_button_pressed() -> void:
+	play_button.hide()
+	pause_menu_items.hide()
+	
+	pause_button.show()
+
+	pause_menu_items.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+func _on_pause_button_pressed() -> void:
+	play_button.show()
+	pause_menu_items.show()
+	
+	pause_button.hide()
+	
+	pause_menu_items.mouse_filter = Control.MOUSE_FILTER_STOP
