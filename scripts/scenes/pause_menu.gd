578fcd5d1a51c2c7020e7db60f69764cb745f6f9
Vaccaria <msbbomisol@gmail.com>
2025-07-14 23:16:59 +0800

    feat: added animation for pause menu

--- scripts/scenes/pause_menu.gd
+++ scripts/scenes/pause_menu.gd
@@ -7,21 +7,33 @@ extends Control
 @onready var pause_button: Button = $PauseButton
 @onready var pause_menu_items: Control = $PauseMenuItems
 
-func _ready() -> void:
-	_on_play_button_pressed()	
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _on_play_button_pressed() -> void:
+func _ready() -> void:
 	play_button.hide()
 	pause_menu_items.hide()
-	
 	pause_button.show()
 
+func _on_play_button_pressed() -> void:
+	if animation_player.is_playing(): return
+	
+	play_button.hide()
+	pause_button.show()
+
+	animation_player.play_backwards("show_menu")
+	await animation_player.animation_finished
+
+	pause_menu_items.hide()
 	pause_menu_items.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 func _on_pause_button_pressed() -> void:
+	if animation_player.is_playing(): return
+	
 	play_button.show()
 	pause_menu_items.show()
-	
 	pause_button.hide()
+
+	animation_player.play("show_menu")
+	await animation_player.animation_finished
 	
 	pause_menu_items.mouse_filter = Control.MOUSE_FILTER_STOP
