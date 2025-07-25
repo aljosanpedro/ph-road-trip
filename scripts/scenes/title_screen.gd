6be79f395190f14f0122ac6314d826dbf8d8ef2f
Vaccaria <msbbomisol@gmail.com>
2025-07-26 01:19:54 +0800

    fix(title screen): adjusted x position of buttons

--- scripts/scenes/title_screen.gd
+++ scripts/scenes/title_screen.gd
@@ -25,6 +25,12 @@ func _on_start_game_pressed() -> void:
 	await animation_player.animation_finished
 	get_tree().change_scene_to_packed(preload("res://scenes/scenes/game_main.tscn"))
 
+func _on_quit_game_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
 func _activate_buttons(value: bool) -> void:
 	for game_button: Button in main_menu_buttons.get_children():
 		game_button.use_parent_material = value
@@ -35,4 +41,3 @@ func _title_button_focus_entered(game_button: Button) -> void:
 func _title_button_focus_exited(game_button: Button) -> void:
 	game_button.use_parent_material = true
 		
-	
